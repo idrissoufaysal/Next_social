6a6aea71452584e3496ad1ed63ce91bd3efb80bf
idrissou <idrissoufaysal93@gmail.com>
2024-09-04 16:41:15 +0000

    a

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,11 +1,27 @@
 import { PrismaClient } from "@prisma/client";
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export const GET = async (res: NextApiResponse) => {
-  const users = await prisma.user.findMany();
-  return   JSON.stringify(users)
+export const GET = async () => {
+  try {
+    const users = await prisma.user.findMany();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    console.log(error.message);
+  }
 };
 
-
+export const POST = async (req: NextApiRequest) => {
+  
+  try {
+    const user = await prisma.user.create({
+      data: JSON.parse(req.body),
+    });
+    
+    return new NextResponse(JSON.stringify(user), { status: 201 });
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
